3b7612efed794a095ad5263eb56a957f94250440
Yaroslav Smirnov <smirnovyaroslav.ua@gmail.com>
2017-02-09 13:00:15 +0200

    eig_separation

--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,41 @@
 ###################################################################################################
 #   feature preprocessing
 ###################################################################################################
-# pp.new_features( patient_test[0], start_time, step_time,features)
+# pp.new_features( patient_test[0], f_window_size, f_window_step,features)
+# interictal_w,interictal_v,preictal_w,preictal_v = pp.eig_separation(patients,
+#                                                                     eig = True,
+#                                                                     path_features = 'D:/Diploma/Features/f_',
+#                                                                     path_csv = 'D:/Diploma/Signals/',
+#                                                                     f_window_size = 300,
+#                                                                     f_window_step = 60,
+#                                                                     eig_window_size = 4,
+#                                                                     eig_num = 1)
 ###################################################################################################
 import numpy as np
 import pandas as pd
 import math as m
-import load as l
+import load as ld
 
-def new_features( test, start_time, step_time,features):
-	feature = preprocessing(path='D:/Diploma/Signals/Ready_Signals/' + test + '.csv',start_time=start_time,step_time=step_time,features = features)
-	feature.to_csv('D:/Diploma/Signals/Features/f_' + test + '.csv', mode = 'w',index=False)
-	writer = pd.ExcelWriter('D:/Diploma/Signals/Features/f_' + test + '.xlsx')
+def new_features( path_source, path_feature_csv,path_feature_xlsx, f_window_size, f_window_step,features):
+	feature = preprocessing(path_source,f_window_size=f_window_size,f_window_step=f_window_step,features = features)
+	feature = feature.T
+	feature.to_csv(path_feature_csv, mode = 'w',index=False)
+	writer = pd.ExcelWriter(path_feature_xlsx)
 	feature.to_excel(writer,'Sheet1')
 	writer.save()
 
-def preprocessing(path, start_time, step_time,features):
+def preprocessing(path, f_window_size, f_window_step,features):
 	window = dict()
 	window_Hz = dict()
 	feature = dict()
 	index=[]
-	load = l.load_data(path,data_type=['RR_raw','RR_pos'])
-	s_add = l.step_add(path,data_type='RR_pos',start_time=start_time,step_time=step_time)
-	s_del = l.step_del(path,data_type='RR_pos',step_time=step_time)
+	load = ld.load_data(path,data_type=['RR_raw','RR_pos'])
+	s_add = ld.step_add(path,data_type='RR_pos',f_window_size=f_window_size,f_window_step=f_window_step)
+	s_del = ld.step_del(path,data_type='RR_pos',f_window_step=f_window_step)
 
-	load_Hz = l.load_data(path,data_type=['RR_2Hz','time_2Hz'])
-	s_add_Hz = l.step_add(path,data_type='time_2Hz',start_time=start_time,step_time=step_time)
-	s_del_Hz = l.step_del(path,data_type='time_2Hz',step_time=step_time)
+	load_Hz = ld.load_data(path,data_type=['RR_2Hz','time_2Hz'])
+	s_add_Hz = ld.step_add(path,data_type='time_2Hz',f_window_size=f_window_size,f_window_step=f_window_step)
+	s_del_Hz = ld.step_del(path,data_type='time_2Hz',f_window_step=f_window_step)
 
 	load(item = window, step_add=s_add.__next__(),step_del=s_del.__next__(),mode='first')
 	load_Hz(item = window_Hz, step_add=s_add_Hz.__next__(),step_del=s_del_Hz.__next__(),mode='first')
@@ -126,6 +135,93 @@ def feature_FDF(item,arrays):
 		item['HF']=[sum(powerHF)/len(powerHF)]
 		item['LF/HF']=[item['LF'][-1]/item['HF'][-1]]
 
+def eig_separation(patients,
+				   eig = True,
+				   path_features = 'D:/Diploma/Features/f_',
+				   path_csv = 'D:/Diploma/Signals/',
+				   f_window_size = 300,
+				   f_window_step = 60,
+				   eig_window_size = 4,
+				   eig_num = 1):
+	interictal_time = list()
+	interictal_feature = list()
+	preictal_time = list()
+	preictal_feature = list()
+	interictal_w = list()
+	interictal_v = list()
+	preictal_w = list()
+	preictal_v = list()
+	for patient in patients:
+		path_feature_csv = path_features + patient + '.csv'
+		path_patient     = path_csv + patient + '.csv'
+
+		patient_feature = pd.read_csv(path_feature_csv, header=0)
+		try:
+			seizure_start = ld.load_column(path_patient, data_type='seizureStart')['seizureStart']
+			seizure_stop  = ld.load_column(path_patient, data_type='seizureEnd')['seizureEnd']
+			seizure_start_time = seizure_start[0]
+			seizure_stop_time  = seizure_stop[0]
+		except:
+			seizure_start_time = patient_feature.values[0, -1]
+			seizure_stop_time  = patient_feature.values[0, -1]
+
+		seizure_counter = 0
+		break_counter = 0
+		interictal_counter = 0
+		preictal_counter   = 0
+		for i in range(patient_feature.shape[1]):
+			break_counter -=1
+			if break_counter > 0:
+				continue
+			if(patient_feature.values[0, i] - patient_feature.values[0, i-1]) > f_window_size:
+				break_counter = int(f_window_size/f_window_step)
+				continue
+
+			if (seizure_start_time - patient_feature.values[0, i]) > f_window_size :
+				interictal_counter += 1
+				preictal_counter    = 0
+				interictal_time.append(patient_feature.values[0,i])
+				interictal_feature.append(patient_feature.values[2:,i])
+				if eig:
+					if interictal_counter > (eig_window_size - 1):
+						window_feature = np.array(interictal_feature[-eig_window_size:])
+						covmat = np.cov(window_feature.T)
+						wn, vn = np.linalg.eig(covmat)
+						interictal_w.append(float(wn[eig_num]))
+						interictal_v.append(vn[:, eig_num])
+
+			else:
+				if((seizure_start_time - patient_feature.values[0, i]) < f_window_size) & ((seizure_start_time - patient_feature.values[0, i]) > 0):
+					interictal_counter = 0
+					preictal_counter  += 1
+					preictal_time.append(patient_feature.values[0,i])
+					preictal_feature.append(patient_feature.values[2:,i])
+
+					if eig:
+						if preictal_counter > (eig_window_size - 1):
+							window_feature = np.array(preictal_feature[-eig_window_size:])
+							covmat = np.cov(window_feature.T)
+							wn, vn = np.linalg.eig(covmat)
+							preictal_w.append(float(wn[eig_num]))
+							preictal_v.append(vn[:, eig_num])
+
+				if(seizure_stop_time - patient_feature.values[0, i]) < -f_window_step:
+					seizure_counter += 1
+
+					if seizure_counter == seizure_start.__len__():
+						seizure_start_time = patient_feature.values[0, -1] + 2*f_window_size
+						seizure_stop_time  = patient_feature.values[0, -1] + 2*f_window_size
+					else:
+						seizure_start_time = seizure_start[seizure_counter]
+						seizure_stop_time  = seizure_stop [seizure_counter]
+	interictal_v = np.array(interictal_v)
+	interictal_w = np.array(interictal_w)
+	preictal_v   = np.array(preictal_v)
+	preictal_w   = np.array(preictal_w)
+	if eig:
+		return interictal_w,interictal_v,preictal_w,preictal_v
+	else:
+		return interictal_time,interictal_feature,preictal_time,preictal_feature
 
 
 
